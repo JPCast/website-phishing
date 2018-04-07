9ef2d41b6d3fea3d6f67b58c5ec6505d88ef09b5
JPCast <jose.pedro.cast@gmail.com>
2018-04-07 13:14:53 +0100

    Logistic regression with gradient descent working

--- logReg.m
+++ logReg.m
@@ -14,6 +14,7 @@ c9 = data(:, 9);
 
 X = [c1 c2 c3 c4 c5 c6 c7 c8 c9];
 y = data(:, 10);
+y = y + 2; % map labels to interval 1 to 3
 
 %Divide data into training and test
 train_x = X([1:946],:); %First 946 rows for training (aproximatly 70% of the dataset)
@@ -30,21 +31,30 @@ test_y = y([947:1353],:);
 [m, n] = size(train_x); % m training examples, n features
 
 % Add extra column of 1 to X
-column_ones = ones(m, 1);
-train_x = [column_ones train_x];
+%column_ones = ones(m, 1);
+%train_x = [column_ones train_x];
 
 % Initialize fitting parameters =0
-theta = zeros(n+1, 1); % theta is a column vector with the number of features plus 1
-iterations = 1500;
-alpha = 0.01;
+%theta = zeros(n+1, 1); % theta is a column vector with the number of features plus 1
+%iterations = 1500;
+%alpha = 0.01;
 
 % run gradient descent
-[theta] = gradientDescent(train_x, train_y, theta, alpha, iterations);
+%[theta] = gradientDescent(train_x, train_y, theta, alpha, iterations);
 
 % print theta to screen
-fprintf('Theta found by gradient descent: ');
-fprintf('%f \n', theta);
+%fprintf('Theta found by gradient descent: ');
+%fprintf('%f \n', theta);
+
+
+[theta] = oneVsAll(train_x, train_y, 3, 0.1); % Get thetas for all 
+                                            % classifiers
+
 
 
 %% ========== Part 3: Check hypothesis performance ===========
-p = predictOneVsAll(theta, test_x);
+pred = predictOneVsAll(theta, test_x);
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(pred (:) == test_y(:)) * 100);
+
+
