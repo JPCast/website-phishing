aff41a8d4a8cb0068f1a7734002901fa7d49bbc7
Pedro Santos <pedroamaralsantos@ua.pt>
2018-05-06 15:52:13 +0100

    added code to calculate models using the features most important

--- python/featureImportance.py
+++ python/featureImportance.py
@@ -1,16 +1,51 @@
 from numpy import loadtxt
 from xgboost import XGBClassifier
 from xgboost import plot_importance
+from sklearn.model_selection import train_test_split
 from matplotlib import pyplot
-# load data
-dataset = loadtxt('PhishingData.txt', delimiter=",")
-print(len(dataset[0]))
+from sklearn.metrics import accuracy_score
+from numpy import sort
+from sklearn.feature_selection import SelectFromModel
+
+
+data = loadtxt('PhishingData.txt', delimiter=",")
+
 # split data into X and y
-X = dataset[:,0:8]
-y = dataset[:,9]
+X = data[:,0:8]
+y = data[:,9]
+
+seed = 7
+test_size = 0.3
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
+
 # fit model no training data
 model = XGBClassifier()
-model.fit(X, y)
-# plot feature importance
+#print(model)
+model.fit(X_train, y_train)
+# plot the importance of feautures
 plot_importance(model)
 pyplot.show()
+
+
+#make predictions
+#y_pred = model.predict(X_test)
+#predictions = [round(value) for value in y_pred]
+#verify predictions
+#accuracy = accuracy_score(y_test, predictions)
+#print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+########################################################train data by test each subset of features by importance.
+thresholds = sort(model.feature_importances_)
+for thresh in thresholds:
+	# select features using threshold
+	selection = SelectFromModel(model, threshold=thresh, prefit=True)
+	select_X_train = selection.transform(X_train)
+	# train model
+	selection_model = XGBClassifier()
+	selection_model.fit(select_X_train, y_train)
+	# eval model
+	select_X_test = selection.transform(X_test)
+	y_pred = selection_model.predict(select_X_test)
+	predictions = [round(value) for value in y_pred]
+	accuracy = accuracy_score(y_test, predictions)
+	print("Thresh=%.3f, n=%d, Accuracy: %.2f%%" % (thresh, select_X_train.shape[1], accuracy*100.0))
